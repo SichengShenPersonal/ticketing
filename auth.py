61f51bdf5aa4aed19a866a18047f78087e2d56ad
SichengShenPersonal <sichengshenpersonal@gmail.com>
2025-05-23 18:38:15 -0400

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,5 @@
+USER_DB = {
+    "admin_user": {"level": "admin", "groups": ["市场部", "技术部", "行政部"]},
+    "agent_1":    {"level": "agent", "groups": ["技术部"]},
+    "viewer_1":   {"level": "viewer", "groups": ["行政部"]}
+}
